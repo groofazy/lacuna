04c903a429f05dede574c2df66db1dd4aa795128
Ryan <gabe.celtics0@gmail.com>
2025-04-23 22:50:13 -0700

    implemented the following functions: get_album_tracks, print_artists_top_tracks_and_popularity, get_artists_albums_ids

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from auth import get_token
-from spotify_api_logic import search_for_artist, get_songs_by_artist
-
+from spotify_api_logic import search_for_artist, get_artists_top_tracks, get_artists_albums, print_top_tracks_and_popularity
 token = get_token()
 
 artist_name = input("Enter artist name: ")
-
 result = search_for_artist(token, artist_name)
-artist_id = result["id"] 
-songs = get_songs_by_artist(token, artist_id)
+artist_id = result["id"]
+top_tracks = get_artists_top_tracks(token, artist_id)
+artists_albums =  get_artists_albums(token, artist_id)
 
-for idx, song in enumerate(songs):
-    print(f"{idx + 1} , {song['name']}")
+print_top_tracks_and_popularity(token, artist_id)
--- spotify_api_logic.py
+++ spotify_api_logic.py
@@ -1,8 +1,6 @@
 from requests import get
-from auth import get_token, get_auth_header
+from auth import get_auth_header
 import json
-
-token = get_token()
 
 # function to search for artist
 def search_for_artist(token, artist_name):
@@ -19,19 +17,59 @@ def search_for_artist(token, artist_name):
     
     return json_result[0]
 
-def get_songs_by_artist(token, artist_id):
+# function to get songs from artist
+def get_artists_top_tracks(token, artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=CA" # look for specific artist and their top tracks
     headers = get_auth_header(token)
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["tracks"]
     return json_result 
 
-# result = search_for_artist(token, "Malcom Todd")
-# artist_id = result["id"] 
-# songs = get_songs_by_artist(token, artist_id)
+# function to get albums from specific artist
+def get_artists_albums(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/albums?limit=10" # request for artist's albums, top 10
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["items"]
+    return json_result
 
-# for idx, song in enumerate(songs):
-#     print(f"{idx + 1}, {song['name']}")
+def get_artists_albums_ids(token, artist_id):
+    albums = get_artists_albums(token, artist_id)
+    album_ids = []
+
+    for idx, album in enumerate(albums):
+        # print(f"{idx + 1}, ID: {album['id']}")
+        album_ids.append(album['id'])
+    
+    return album_ids
+
+def get_artists_top_tracks_popularity(token, artist_id):
+    top_tracks = get_artists_top_tracks(token, artist_id)
+    pop_score = []
+
+    for idx, song in enumerate(top_tracks):
+        # print(f"{idx + 1}, {song['name']} has a popularity score of {song['popularity']}")
+        pop_score.append(song['popularity']) # stores popularity score of top 10 tracks into an array
+    
+    return pop_score
+
+def print_top_tracks_and_popularity(token, artist_id):
+    top_tracks = get_artists_top_tracks(token, artist_id)
+
+    for idx, track in enumerate(top_tracks):
+        print(f"{idx +1}. {track['name']} has a popularity score of {track['popularity']}")
 
 
-# result["name"] gets artist name
+def get_album_tracks(token, artist_id):
+    album_ids = get_artists_albums_ids(token, artist_id)
+    track_names = []
+    headers = get_auth_header(token)
+    for idx, album_id in enumerate(album_ids):
+        url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
+        result = get(url, headers=headers)
+        album_items = json.loads(result.content)["items"] # get items of each album
+
+        for idx, album_item in enumerate(album_items):
+            track_names.append(album_item['name'])
+    
+    return track_names
