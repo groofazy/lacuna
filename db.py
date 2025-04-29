779047788d04ad48cabdbe7b0403d51b93ed298f
Ryan <gabe.celtics0@gmail.com>
2025-04-28 20:20:39 -0700

    created two functions: initialize_db() and get_all_artists(). all functions now have con and cur as local variables

--- db.py
+++ db.py
@@ -1,17 +1,30 @@
 import sqlite3
 
-con = sqlite3.connect('spotify.db')  # Creates a new database file if it doesn’t exist
-cur = con.cursor()
-
-# create table statement
-cur.execute("CREATE TABLE IF NOT EXISTS artist(name, num_albums, popularity)") # include if not exists to bypass the "table already exists" error message
+def initalize_db():
+    con = sqlite3.connect('spotify.db')
+    cur = con.cursor()
+    # create table statement
+    cur.execute("CREATE TABLE IF NOT EXISTS artist(name, num_albums, popularity)") # include if not exists to bypass the "table already exists" error message
 
 def insert_artist(name, num_albums, popularity, con):
+    con = sqlite3.connect('spotify.db')
+    # cur = con.cursor()
     with con:
         con.execute("INSERT INTO artist(name, num_albums, popularity) VALUES(?, ?, ?)", (name, num_albums, popularity))
     con.commit()
+    con.close()
 
-for row in cur.execute("SELECT name, num_albums, popularity FROM artist"):
-    print(row)
+def get_all_artists():
+    con = sqlite3.connect('spotify.db')
+    cur = con.cursor()
+    
+    cur.execute("SELECT name, num_albums, popularity FROM artist")
+    rows = cur.fetchall()
+    con.close()
+    return rows
 
-con.close()
+def print_artists_data():
+    con = sqlite3.connect('spotify.db')
+    cur = con.cursor()
+    for row in cur.execute("SELECT name, num_albums, popularity FROM artist"):
+        print(row)
