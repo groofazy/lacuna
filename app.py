3c498046c4aead4e0d28a7bb23d1e2855e9a798f
Ryan <gabe.celtics0@gmail.com>
2025-04-28 22:51:36 -0700

    fixed problem in app.py where the if statement that returns a json error response status code was using the real_name variable without it being defined in its scope. i also had to add con and cur connections, since the db.insert_artist function requires con as a parameter in the function

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from markupsafe import escape
 import db
 import spotify_api_logic
 from auth import get_token
+import sqlite3
 
 app = Flask(__name__)
 
@@ -37,9 +38,12 @@ def artists_get():
     return jsonify(artists)
 
 def artists_post():
+    con = sqlite3.connect('spotify.db')
+    cur = con.cursor()
+
     data = request.get_json()
 
-    if not data or 'artist_name' not in data:
+    if not data or 'artist_name' not in data: # returns error code for missing artist name
         return jsonify({"error": "artist_name is required"}), 400
 
     # get the request data
@@ -58,11 +62,14 @@ def artists_post():
     real_name = artist_result['name']
     artist_id = artist_result['id']
 
+    if db.artist_in_db(real_name): # returns error code for duplicate entries
+        return jsonify({"error": f"Artist '{real_name}' already exists in the database."}), 409
+
     num_albums = spotify_api_logic.get_num_artist_albums(token, artist_id)
     avg_popularity = spotify_api_logic.get_avg_pop_score(token, artist_id)
 
     # insert into database
-    db.insert_artist(real_name, num_albums, avg_popularity)
+    db.insert_artist(real_name, num_albums, avg_popularity, con)
 
     return jsonify({"message": f"Artist '{real_name}' added successfully."}), 201
 
